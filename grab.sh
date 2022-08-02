4f0103b95aa4a10b7255f47aba74a327921e97e2
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-08-02 08:40:38 -0300

    Make local opal and ace

---
+++ grab.sh
@@ -0,0 +1,10 @@
+cd web
+wget "https://cdn.opalrb.com/opal/current/opal.js"
+wget "https://cdn.opalrb.com/opal/current/native.js"
+
+wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/ace.min.js"
+wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/mode-ruby.min.js"
+wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/theme-cobalt.min.js"
+wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/ext-settings_menu.min.js"
+wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/keybinding-vim.min.js"
+cd ..
