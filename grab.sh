739877a0e2b67ab307f117d8fcee39f8e91cb3fb
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-12-01 23:24:59 -0300

    Add conditional CDNs load

--- grab.sh
+++ grab.sh
@@ -1,11 +1,19 @@
-echo Grabing CDNs
-cd web
-wget "https://cdn.opalrb.com/opal/current/opal.js"
-wget "https://cdn.opalrb.com/opal/current/native.js"
+echo Grabing needed CDNs
 
-wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/ace.min.js"
-wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/mode-ruby.min.js"
-wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/theme-cobalt.min.js"
-wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/ext-settings_menu.min.js"
-wget "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/keybinding-vim.min.js"
+function loadCDN(){
+    url=$1;
+    if [ ! -f "${url##*/}" ]; then
+        wget "$1";
+    fi
+}
+
+cd web
+loadCDN "https://cdn.opalrb.com/opal/current/opal.js"
+loadCDN "https://cdn.opalrb.com/opal/current/native.js"
+
+loadCDN "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/ace.min.js"
+loadCDN "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/mode-ruby.min.js"
+loadCDN "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/theme-cobalt.min.js"
+loadCDN "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/ext-settings_menu.min.js"
+loadCDN "https://cdnjs.cloudflare.com/ajax/libs/ace/1.8.1/keybinding-vim.min.js"
 cd ..
