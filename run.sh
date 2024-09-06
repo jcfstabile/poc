8f615e5dea0f8831df5935369fd541f5cba1a17a
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2024-09-06 03:34:28 -0300

    Add stop script to stop server

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 cd web;
 sh -c "../grab.sh";
 sh -c "../server.py&";
+export SERVERPID=$(ps ax | grep "python3 ../[s]erver.py" | awk '{print $1}')
+
+echo kill $SERVERPID > ../stop.sh
+
 xdg-open "http://localhost:8000/";
+
+# kill $SERVERPID
+
