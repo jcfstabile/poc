d13017aab27ac48663add879c48eb54a0b0f0e32
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2025-03-18 22:13:56 -0300

    Make stop.sh executable and self destructing

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 cd web;
 sh -c "../scripts/grab.sh";
 sh -c "../scripts/server.py&";
-export SERVERPID=$(ps ax | grep "python3 ../[s]erver.py" | awk '{print $1}')
+export SERVERPID=$(ps ax | grep "python3 ../scripts/[s]erver.py" | awk '{print $1}')
 
-echo kill $SERVERPID > ../stop.sh
+echo "if ps -p $SERVERPID > /dev/null ; then kill $SERVERPID; rm -- \$0; fi" > ../stop.sh
+chmod +x ../stop.sh
 
 xdg-open "http://localhost:8000/";
 
